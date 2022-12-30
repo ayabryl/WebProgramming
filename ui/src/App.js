670c364335c2d36c4d38c1b2ef70f2b22687ece3
shahar <shaharl44@gmail.com>
2022-12-29 20:17:34 -0800

    Create basic nevigation

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,7 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+
 import AuthPage from "./pages/AuthPage";
+import Layout from "./components/Layout/Layout";
+import HomePage from "./pages/HomePage";
+import Profile from "./pages/Profile";
+import Cart from "./pages/Cart";
 
 function App() {
-  return <AuthPage />;
+  // return <AuthPage />;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" exact element={<HomePage />}></Route>
+        <Route path="/auth" exact element={<AuthPage />}></Route>
+        <Route path="/profile" exact element={<Profile />}></Route>
+        <Route path="/cart" exact element={<Cart />}></Route>
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
