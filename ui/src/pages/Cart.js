53ba7f611df53fb80b8ca5e21de0bda694131ff8
shahar <shaharl44@gmail.com>
2023-01-16 22:16:42 -0800

    fix create order

--- ui/src/pages/Cart.js
+++ ui/src/pages/Cart.js
@@ -1,25 +1,22 @@
-import Cart from "../components/cart/Cart"
-import {useNavigate } from "react-router-dom";
-
+import Cart from "../components/cart/Cart";
+import { useNavigate } from "react-router-dom";
 
 const CartPage = () => {
   const navigate = useNavigate();
-  const handleOrder = (order) => { 
+  const handleOrder = (order) => {
     const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(order)
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(order),
     };
-    fetch('http://localhost:3001/addOrder', requestOptions)
-      .then(response => {
+    fetch("http://localhost:3001/addOrder", requestOptions)
+      .then((response) => {
         console.log(response);
-        navigate('/')
-      }
-      ).catch((error) => console.log(error))
-  }
-  return (
-    <Cart handleOrder={handleOrder}></Cart>
-  );
+        navigate("/");
+      })
+      .catch((error) => console.log(error));
+  };
+  return <Cart handleOrder={handleOrder}></Cart>;
 };
 
 export default CartPage;
