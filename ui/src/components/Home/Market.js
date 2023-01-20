3314f84f0c57f2e1eb6ef9cbdcb687112c8fcce8
shahar <shaharl44@gmail.com>
2023-01-20 13:27:18 -0800

    pagination in admin

--- ui/src/components/Home/Market.js
+++ ui/src/components/Home/Market.js
@@ -1,95 +1,20 @@
 import { useState, useEffect, useContext, Fragment } from "react";
-import { Grid, Typography, IconButton, Box } from "@mui/material";
+import { Grid, TablePagination } from "@mui/material";
 
 import { useNavigate } from "react-router-dom";
-import toast, { Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import axios from "axios";
 
 import ProductCard from "./ProductCard";
 import SearchContext from "../../contexts/SearchContext";
 import SearchBar from "../Layout/SearchBar";
-import FirstPageIcon from "@mui/icons-material/FirstPage";
-import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
-import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
-import LastPageIcon from "@mui/icons-material/LastPage";
-import PropTypes from "prop-types";
-import { useTheme } from "@mui/material/styles";
-import TablePagination from "@mui/material/TablePagination";
-
-function TablePaginationActions(props) {
-  const theme = useTheme();
-  const { count, page, rowsPerPage, onPageChange } = props;
-
-  const handleFirstPageButtonClick = (event) => {
-    onPageChange(event, 0);
-  };
-
-  const handleBackButtonClick = (event) => {
-    onPageChange(event, page - 1);
-  };
-
-  const handleNextButtonClick = (event) => {
-    onPageChange(event, page + 1);
-  };
-
-  const handleLastPageButtonClick = (event) => {
-    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-  };
-
-  return (
-    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
-      <IconButton
-        onClick={handleFirstPageButtonClick}
-        disabled={page === 0}
-        aria-label="first page"
-      >
-        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
-      </IconButton>
-      <IconButton
-        onClick={handleBackButtonClick}
-        disabled={page === 0}
-        aria-label="previous page"
-      >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowRight />
-        ) : (
-          <KeyboardArrowLeft />
-        )}
-      </IconButton>
-      <IconButton
-        onClick={handleNextButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="next page"
-      >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowLeft />
-        ) : (
-          <KeyboardArrowRight />
-        )}
-      </IconButton>
-      <IconButton
-        onClick={handleLastPageButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="last page"
-      >
-        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
-      </IconButton>
-    </Box>
-  );
-}
-
-TablePaginationActions.propTypes = {
-  count: PropTypes.number.isRequired,
-  onPageChange: PropTypes.func.isRequired,
-  page: PropTypes.number.isRequired,
-  rowsPerPage: PropTypes.number.isRequired,
-};
+import TablePaginationActions from "../Layout/TablePaginationAction";
 
 const Market = () => {
   const [products, setProducts] = useState([]);
   const [parsedProduct, setParsedProducts] = useState([]);
   const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [rowsPerPage, setRowsPerPage] = useState(16);
   const { searchWord } = useContext(SearchContext);
   const navigate = useNavigate();
 
@@ -137,10 +62,6 @@ const Market = () => {
     setParsedProducts(productsShow);
   }, [searchWord, products]);
 
-  // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - parsedProduct.length) : 0;
-
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
@@ -180,14 +101,14 @@ const Market = () => {
         sx={{ position: "absolute", bottom: 0 }}
       >
         <TablePagination
-          rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+          rowsPerPageOptions={[16, 32, 48, { label: "All", value: -1 }]}
           colSpan={3}
           count={parsedProduct.length}
           rowsPerPage={rowsPerPage}
           page={page}
           SelectProps={{
             inputProps: {
-              "aria-label": "rows per page",
+              "aria-label": "products per page",
             },
             native: true,
           }}
--- ui/src/components/admin/AdminAllProducts.js
+++ ui/src/components/admin/AdminAllProducts.js
@@ -1,17 +1,18 @@
 import { useState, useEffect, useContext, Fragment } from "react";
-import { Grid } from "@mui/material";
+import { Grid, TablePagination } from "@mui/material";
 
-import { Toaster } from "react-hot-toast";
 import axios from "axios";
 
 import ProductCard from "../Home/ProductCard";
 import SearchContext from "../../contexts/SearchContext";
 import SearchBar from "../Layout/SearchBar";
-import { StyledButtonContained, theme } from "../../theme";
+import TablePaginationActions from "../Layout/TablePaginationAction";
 
 const AdminAllProducts = () => {
   const [products, setProducts] = useState([]);
   const [parsedProduct, setParsedProducts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(12);
   const { searchWord } = useContext(SearchContext);
 
   const fetchData = () => {
@@ -48,6 +49,15 @@ const AdminAllProducts = () => {
     setParsedProducts(productsShow);
   }, [searchWord, products]);
 
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   return (
     <Fragment>
       <Grid
@@ -60,8 +70,37 @@ const AdminAllProducts = () => {
           <SearchBar />
         </Grid>
         <Grid container spacing={4} sx={{ p: 3 }}>
-          {parsedProduct}
+          {rowsPerPage > 0
+            ? parsedProduct.slice(
+                page * rowsPerPage,
+                page * rowsPerPage + rowsPerPage
+              )
+            : parsedProduct}
         </Grid>
+      </Grid>
+      <Grid
+        width="98%"
+        alignItems="center"
+        display="flex"
+        justifyContent="center"
+        sx={{ position: "absolute", bottom: 0 }}
+      >
+        <TablePagination
+          rowsPerPageOptions={[12, 16, 32, { label: "All", value: -1 }]}
+          colSpan={3}
+          count={parsedProduct.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          SelectProps={{
+            inputProps: {
+              "aria-label": "products per page",
+            },
+            native: true,
+          }}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+          ActionsComponent={TablePaginationActions}
+        />
       </Grid>
     </Fragment>
   );
